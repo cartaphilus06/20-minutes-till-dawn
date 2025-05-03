41c47a01e284926a19678ea7bc33601f66361ec5
Mahed <ghasdak2220@gmail.com>
2025-05-03 22:05:00 +0330

    added some shits

---
+++ core/src/main/java/com/tilldawn/Models/User/User.java
@@ -0,0 +1,25 @@
+package com.tilldawn.Models.User;
+
+import java.util.*;
+
+public class User {
+    private static final ArrayList<User> users = new ArrayList<User>();
+    private String username;
+    private String password;
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
