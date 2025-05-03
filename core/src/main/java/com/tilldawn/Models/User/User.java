16ef456b6ed2f961815cbad1221433c74da5fd7c
Mahed <ghasdak2220@gmail.com>
2025-05-03 22:07:18 +0330

    user updated

--- core/src/main/java/com/tilldawn/Models/User/User.java
+++ core/src/main/java/com/tilldawn/Models/User/User.java
@@ -3,12 +3,19 @@ package com.tilldawn.Models.User;
 import java.util.*;
 
 public class User {
-    private static final ArrayList<User> users = new ArrayList<User>();
+    private static final ArrayList<User> allUsers = new ArrayList<User>();
     private String username;
     private String password;
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+        allUsers.add(this);
+    }
+    public static User getUser(String username) {
+        for(User user : allUsers) {
+            if(user.getUsername().equals(username)) return user;
+        }
+        return null;
     }
     public String getUsername() {
         return username;
